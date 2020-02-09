2ce61a7f8f58602bf195a2f945e4ad8d1e6e8b96
李鹏飞 <lipengfei19529@163.com>
2020-02-09 10:21:50 +0800

    加入读取application.properties以外的配置文件的测试

--- src/main/java/com/springbootredis/redis/config/CommonConfig.java
+++ src/main/java/com/springbootredis/redis/config/CommonConfig.java
@@ -2,17 +2,29 @@ package com.springbootredis.redis.config;
 
 
 import com.springbootredis.redis.model.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 
 @Configuration
 @EnableCaching
+@PropertySource({"classpath:redis.properties","classpath:config/myTest.properties"})
 public class CommonConfig {
+    @Value("${im.hs.server.redis.sentinel1.host}")
+    private String host;
+
+    @Value("${userTest.name}")
+    private String name;
+
+
     @Bean(name = "user")
     public Object User(){
-        return new User();
+        System.out.println(host);
+        User user = new User();
+        return user;
     }
 
 }
